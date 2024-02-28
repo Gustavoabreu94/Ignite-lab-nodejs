58a27d37d18343be27422f656bbc65b0ca80b5a6
Gustavo Alves Abreu <gustavoalvesabreu94@gmail.com>
2024-02-28 08:36:12 -0300

    create notification database and validations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MailService } from './mail/mail.service';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
   constructor(private readonly appService: AppService,
-              private readonly mailService: MailService) {}
+              private readonly mailService: MailService,
+              private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.mailService.sendEmail();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+
+    await this.prisma.notification.create({
+      data: {
+        content: body.content,
+        category: body.category,
+        recipientId: body.recipientId,
+  }});
+  //   await this.prisma.notification.create({
+  //     data: {
+  //       content: 'Você tem uma nova solicitação de amizade!',
+  //       category: 'social',
+  //       recipientId: randomUUID(),
+  // }});
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailService } from './mail/mail.service';
 import { SMTPMailService } from './mail/smtp-maill.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService,
+  providers: [
+    AppService,
+    PrismaService,
   {
     provide: MailService,
     useClass: SMTPMailService,
